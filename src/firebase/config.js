031c4554509dbce5d6720739109bd13279e4207d
CamilleWS <camille.villa@epitech.eu>
2021-03-24 16:42:04 +0100

    rm some files

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAPVGF84HkVIbRR6DFpMjQ539VawX0AOc8',          //ok
+  apiKey: 'AIzaSyAPVGF84HkVIbRR6DFpMjQ539VawX0AOc8',          // ok
   authDomain: 'teamit-299bf.firebaseapp.com',                 // ok
-  databaseURL: 'teamit-299bf-default-rtdb.firebaseio.com', // ok
-  projectId: 'teamit-299bf',                                        // ok
-  storageBucket: 'teamit-299bf.appspot.com',     // ok
-  messagingSenderId: '358332545849',             // ok
-  appId: '1:358332545849:android:76f42420627b0a46ed46e8', // ok
+  databaseURL: 'teamit-299bf-default-rtdb.firebaseio.com',    // ok
+  projectId: 'teamit-299bf',                                  // ok
+  storageBucket: 'teamit-299bf.appspot.com',                  // ok
+  messagingSenderId: '358332545849',                          // ok
+  appId: '1:358332545849:android:76f42420627b0a46ed46e8',     // ok
 };
 
 if (!firebase.apps.length) {
