0da2857d49335c5ecfe3b013bc96e9728e7af27a
CamilleWS <camille.villa@epitech.eu>
2021-04-04 18:07:40 +0200

    clear branch

--- src/services/redux/reducers/userReducer.js
+++ src/services/redux/reducers/userReducer.js
@@ -15,27 +15,27 @@ const initialState = {
                 },
                 {
                     id:3,
-                    name: 'Jeannine Musk',
-                    avatar_url: 'https://cdn1.iconfinder.com/data/icons/avatar-97/32/avatar-02-512.png',
-                    subtitle: 'Software Ing'
+                    name: 'Gérarldine Lauzier',
+                    avatar_url: 'https://image.flaticon.com/icons/png/512/194/194938.png',
+                    subtitle: 'Webdesigner'
                 },
                 {
                     id:4,
-                    name: 'Martin Dupond',
-                    avatar_url: 'https://i.pinimg.com/564x/17/56/8f/17568fcd478e0699067ca7b9a34c702f.jpg',
-                    subtitle: 'FullStack Dev'
+                    name: 'Grégoire Beaudouin',
+                    avatar_url: 'https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png',
+                    subtitle: 'Community Manager'
                 },
                 {
                     id:5,
-                    name: 'Jeannine Musk',
-                    avatar_url: 'https://cdn1.iconfinder.com/data/icons/avatar-97/32/avatar-02-512.png',
-                    subtitle: 'Software Ing'
+                    name: 'Sabine Paquin',
+                    avatar_url: 'https://audit-controle-interne.com/wp-content/uploads/2019/03/avatar-user-teacher-312a499a08079a12-512x512.png',
+                    subtitle: 'Data Analyst'
                 },
                 {
                     id:6,
-                    name: 'Martin Dupond',
-                    avatar_url: 'https://i.pinimg.com/564x/17/56/8f/17568fcd478e0699067ca7b9a34c702f.jpg',
-                    subtitle: 'FullStack Dev'
+                    name: 'Pierre Lachapelle',
+                    avatar_url: 'https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png',
+                    subtitle: 'Admin System'
                 },
             ],
 };
