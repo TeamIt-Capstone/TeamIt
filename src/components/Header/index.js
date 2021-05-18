1f2dde54d261108c71617b810b1471117a415a19
Loïc POTHIN <loic.pothin@epitech.eu>
2021-05-18 14:46:47 +0400

    Header DONE (need more views to enable all navigations)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,13 +3,19 @@ import { View } from "react-native";
 import { Icon } from "react-native-elements";
 import style from "./style";
 
-export default function Header(props) {
+export default function Header({navigation, scene, insets}) {
+    console.log(navigation);
+    const containerStyle = {
+        ...style.container,
+        marginTop: insets.top
+    };
+
     return (
-        <View style={style.container}>
-            <Icon name='rowing'/>
-            <Icon name='rowing'/>
-            <Icon name='rowing'/>
-            <Icon name='rowing'/>
+        <View style={containerStyle}>
+            <Icon name='briefcase' type="feather" color={scene.route.name === "Home" ? "#2BD999" : "black"} onPress={() => navigation.navigate("Home")}/>
+            <Icon name='star' type="feather" color={scene.route.name === "Favorite" ? "#2BD999" : "black"} onPress={() => navigation.navigate("Favorite")}/>
+            <Icon name='message-circle' type="feather" color={scene.route.name === "Messages" ? "#2BD999" : "black"}/>
+            <Icon name='user' type="feather" color={scene.route.name === "Profile" ? "#2BD999" : "black"}/>
         </View>
     );
 }
